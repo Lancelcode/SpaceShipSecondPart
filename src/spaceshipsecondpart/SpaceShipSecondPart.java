ce9f4975d19068a70c7eaf69582c1afdd2e54f6f
Lancelcode <sowrebollo@gmial.com>
2024-09-29 15:32:20 +0100

    Requirements of the part 2 implemented ,custom methods in the main app

--- src/spaceshipsecondpart/SpaceShipSecondPart.java
+++ src/spaceshipsecondpart/SpaceShipSecondPart.java
@@ -1,39 +1,259 @@
 package spaceshipsecondpart;
 
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.concurrent.ArrayBlockingQueue;
-
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.BufferedReader;
-
 import java.util.HashSet;
 import java.util.StringTokenizer;
 
-
 public class SpaceShipSecondPart {
+
+    // Custom ListNode class
+    public static class ListNode<T> {
+        T data;
+        ListNode<T> next;
+
+        public ListNode(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Custom ListQueue class
+    public static class ListQueue<T> implements Queue<T> {
+        private ListNode<T> front;
+        private ListNode<T> rear;
+        private int size;
+
+        public ListQueue() {
+            this.front = null;
+            this.rear = null;
+            this.size = 0;
+        }
+
+        @Override
+        public void enqueue(T item) {
+            ListNode<T> newNode = new ListNode<>(item);
+            if (rear != null) {
+                rear.next = newNode;
+            }
+            rear = newNode;
+            if (front == null) {
+                front = newNode;
+            }
+            size++;
+        }
+
+        @Override
+        public T dequeue() throws UnderFlowException {
+            if (isEmpty()) {
+                throw new UnderFlowException("Queue is empty");
+            }
+            T data = front.data;
+            front = front.next;
+            if (front == null) {
+                rear = null;
+            }
+            size--;
+            return data;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public String toString() {
+            StringBuilder sb = new StringBuilder();
+            ListNode<T> current = front;
+            while (current != null) {
+                sb.append(current.data).append(" ");
+                current = current.next;
+            }
+            return sb.toString();
+        }
+    }
+
+    // Custom ListStack class
+    public static class ListStack<T> implements Stack<T> {
+        private ListNode<T> top;
+        private int size;
+
+        public ListStack() {
+            this.top = null;
+            this.size = 0;
+        }
+
+        @Override
+        public void push(T item) {
+            ListNode<T> newNode = new ListNode<>(item);
+            newNode.next = top;
+            top = newNode;
+            size++;
+        }
+
+        @Override
+        public T pop() throws UnderFlowException {
+            if (isEmpty()) {
+                throw new UnderFlowException("Stack is empty");
+            }
+            T data = top.data;
+            top = top.next;
+            size--;
+            return data;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+    }
+
+    // Custom ArrayQueue class
+    public static class ArrayQueue<T> implements Queue<T> {
+        private T[] data;
+        private int front;
+        private int rear;
+        private int capacity;
+        private int size;
+
+        @SuppressWarnings("unchecked")
+        public ArrayQueue(int capacity) {
+            this.capacity = capacity;
+            data = (T[]) new Object[capacity];
+            front = 0;
+            rear = -1;
+            size = 0;
+        }
+
+        @Override
+        public void enqueue(T item) {
+            if (size == capacity) {
+                throw new RuntimeException("Queue is full");
+            }
+            rear = (rear + 1) % capacity;
+            data[rear] = item;
+            size++;
+        }
+
+        @Override
+        public T dequeue() throws UnderFlowException {
+            if (isEmpty()) {
+                throw new UnderFlowException("Queue is empty");
+            }
+            T item = data[front];
+            front = (front + 1) % capacity;
+            size--;
+            return item;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+    }
+
+    // Custom ArrayStack class
+    public static class ArrayStack<T> implements Stack<T> {
+        private T[] data;
+        private int top;
+        private int capacity;
+
+        @SuppressWarnings("unchecked")
+        public ArrayStack(int capacity) {
+            this.capacity = capacity;
+            data = (T[]) new Object[capacity];
+            top = -1;
+        }
+
+        @Override
+        public void push(T item) {
+            if (top == capacity - 1) {
+                throw new RuntimeException("Stack is full");
+            }
+            data[++top] = item;
+        }
+
+        @Override
+        public T pop() throws UnderFlowException {
+            if (isEmpty()) {
+                throw new UnderFlowException("Stack is empty");
+            }
+            return data[top--];
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return top == -1;
+        }
+
+        @Override
+        public int size() {
+            return top + 1;
+        }
+    }
+
+    // Queue interface
+    public interface Queue<T> {
+        void enqueue(T item);
+        T dequeue() throws UnderFlowException;
+        boolean isEmpty();
+        int size();
+    }
+
+    // Stack interface
+    public interface Stack<T> {
+        void push(T item);
+        T pop() throws UnderFlowException;
+        boolean isEmpty();
+        int size();
+    }
+
+    // Custom UnderFlowException class
+    public static class UnderFlowException extends Exception {
+        public UnderFlowException(String message) {
+            super(message);
+        }
+    }
 
     // Method to load data from the mission file
     public static String[] loadData(String filename) throws IOException {
-        ArrayList<String> arrList = new ArrayList<>();
+        String[] pods = new String[18]; // Max size
+        int index = 0;
         File myFile = new File(filename);
 
         try (FileReader fr = new FileReader(myFile); BufferedReader br = new BufferedReader(fr)) {
             String data;
 
             // Read the file line by line
-            while ((data = br.readLine()) != null) {
+            while ((data = br.readLine()) != null && index < pods.length) {
                 StringTokenizer st = new StringTokenizer(data, ",");
-                while (st.hasMoreTokens()) {
-                    arrList.add(st.nextToken().trim());
+                while (st.hasMoreTokens() && index < pods.length) {
+                    pods[index++] = st.nextToken().trim();
                 }
             }
         }
-        // If there's an I/O error, we throw it back to be handled in main
-        return arrList.toArray(new String[0]);
+        // Return only the filled portion of the array
+        String[] result = new String[index];
+        System.arraycopy(pods, 0, result, 0, index);
+        return result;
     }
 
     public static void main(String[] args) {
@@ -82,9 +302,10 @@ public class SpaceShipSecondPart {
                 continue; // Skip the rest of the mission if there are issues with pod validation
             }
 
-            Stack<String> container1 = new Stack<>();
-            Stack<String> container2 = new Stack<>();
-            Queue<String> corridor1 = new ArrayBlockingQueue<>(MAX_PODS);
+            // Custom Stack and Queue implementations
+            Stack<String> container1 = new ListStack<>();  // Custom Stack using linked list
+            Stack<String> container2 = new ListStack<>();  // Custom Stack using linked list
+            Queue<String> corridor1 = new ListQueue<>();   // Custom Queue using linked list
 
             System.out.println("\n------------------------------\n");
             System.out.println("\nInitial container1 amount of pods: " + container1.size());
@@ -92,64 +313,52 @@ public class SpaceShipSecondPart {
 
             System.out.println("\n------------------------------\n");
             System.out.println("\nShuttle has reached the space station, the container 1 have " + container1.size() + " amount of pods, container 2 has " + container2.size() + " amount of pods.");
-            System.out.println("\nLoading pods in to containers!");
+            System.out.println("\nLoading pods into containers!");
 
             // Load the first 9 elements into container1
-            for (int i = 0; i < sd.length && container1.size() < 9; i++) {
-                container1.add(sd[i]);
+            for (int i = 0; i < Math.min(9, sd.length); i++) {
+                container1.push(sd[i]);
+            }
+
+            // Load the next 9 elements into container2
+            for (int i = 9; i < Math.min(18, sd.length); i++) {
+                container2.push(sd[i]);
             }
 
             System.out.println("\ncontainer1 size: " + container1.size());
             System.out.println("container2 size: " + container2.size());
 
-            // Load the remaining elements into container2
-            for (int k = 9; k < sd.length && container2.size() < 9; k++) {
-                container2.add(sd[k]);
-            }
-
-            System.out.println("\n===================");
-            System.out.println("\nContainer1 size: " + container1.size());
-            System.out.println("Container2 size: " + container2.size());
             System.out.println("\n===================\n");
 
-            // Proceed with the rest of the logic
+            // Fill the queue with the pods from container1
             System.out.println("\n----------------------------------------------------------");
             System.out.println("\nTransporting pods to the loading bay through the corridor.");
             System.out.println("\n----------------------------------------------------------\n");
 
-            // Transfer elements from containers to the queue (corridor1)
-            while (!container1.empty()) {
-                corridor1.add(container1.pop());
-            }
-
-            System.out.println("\nCorridor contents: " + corridor1);
-
-            while (!container2.empty()) {
-                corridor1.add(container2.pop());
-            }
-
-            System.out.println("\nCorridor contents: " + corridor1);
-
-            ArrayList<String> bay1 = new ArrayList<>();
-            ArrayList<String> bay2 = new ArrayList<>();
-            ArrayList<String> bay3 = new ArrayList<>();
-
-            // Sort elements into the respective bays based on first character
-            while (!corridor1.isEmpty()) {
-                String pod = corridor1.remove();
-                if (pod.charAt(0) == 'P') {
-                    bay1.add(pod);
-                } else if (pod.charAt(0) == 'F') {
-                    bay2.add(pod);
-                } else if (pod.charAt(0) == 'T') {
-                    bay3.add(pod);
+            // Transfer elements from container1 to the queue (corridor1)
+            while (!container1.isEmpty()) {
+                try {
+                    corridor1.enqueue(container1.pop());
+                } catch (UnderFlowException e) {
+                    System.out.println("Failed to pop from container1: " + e.getMessage());
                 }
             }
 
-            // Print contents of each bay
-            System.out.println("\nPersonal Bay: " + bay1);
-            System.out.println("\nFood Bay: " + bay2);
-            System.out.println("\nTechnological Bay: " + bay3);
+            System.out.println("\nCorridor contents: " + corridor1);
+
+            // Transfer elements from container2 to the queue (corridor1)
+            while (!container2.isEmpty()) {
+                try {
+                    corridor1.enqueue(container2.pop());
+                } catch (UnderFlowException e) {
+                    System.out.println("Failed to pop from container2: " + e.getMessage());
+                }
+            }
+
+            System.out.println("\nFinal container1 amount of pods: " + container1.size());
+            System.out.println("Final container2 amount of pods: " + container2.size());
+            System.out.println("\nPods in corridor1: " + corridor1.toString());
+            System.out.println("\nMission: " + mission + " completed successfully.\n");
         }
     }
 }
